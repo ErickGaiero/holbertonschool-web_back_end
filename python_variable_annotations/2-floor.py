ca282d3ce0c368685407c6e94b18132c1da4da8e
ErickGaiero <erickgaiero15@gmail.com>
2025-11-27 03:36:06 +0000

    Exercise 2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
+import math
 """Type-annotated function that returns the floor of a float."""
 
-
-import math
 
 def floor(n: float) -> int:
     """Return the floor of a float."""
