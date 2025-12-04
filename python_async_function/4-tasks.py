e74d1e2539b96601d570765c5d5b21624c69163a
ErickGaiero <erickgaiero15@gmail.com>
2025-12-03 22:03:04 -0300

    Exercise 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -2,12 +2,12 @@
 
 """Execute task_wait_random n times concurrently and return sorted delays."""
 
+from typing import List  # Required for return annotation
 import asyncio
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
-
-async def task_wait_n(n: int, max_delay: int):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Execute task_wait_random n times concurrently and return sorted delays."""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
